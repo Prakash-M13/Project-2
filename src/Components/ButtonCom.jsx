b0a3a6badf0d3107b076b19be62eabba0b5ffcc3
prakash-m13 <oomprakashm13@gmail.com>
2026-01-07 15:29:12 +0530

    Update vit config and components

--- src/Components/ButtonCom.jsx
+++ src/Components/ButtonCom.jsx
@@ -5,6 +5,7 @@ export default function ButtonCom() {
   return (
       <div>
         <Button href='https://www.instagram.com/' style={{backgroundImage:"URL(Capture0.png)",width:"110px",height:"105px"}} variant='contained' color='inherit'></Button> <hr />
+        <h1>Hello</h1>
     </div>
   )
 }
